2d8cd42bd0fe848d4e48a70c14781c76a2673dc2
1F10230017 <s1f102300177@iniad.org>
2024-07-04 11:57:30 +0900

    修正

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
+
 def index(request):
     if request.method == 'POST':
         task = Task(
@@ -20,3 +21,4 @@ def index(request):
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
+
